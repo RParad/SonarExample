3934cdd99cadf8539438ce22c6112f9ad1bee83f
Ryan <Ryan2@MacBook-Air.local>
2021-10-18 12:52:26 +0100

    changes

--- src/test/java/BookTest/BookTest.java
+++ src/test/java/BookTest/BookTest.java
@@ -10,8 +10,25 @@ public class BookTest {
 
         @Test
         public void testAuthorMethod() {
-            Book testBook = new Book("Poe", "fantasy", "Fiction");
-            assertEquals("The book has an author", "Poe", testBook.getAuthor());
+            Book testAuthor = new Book("Poe", "fantasy", "Fiction");
+            assertEquals("The book has an author", "Poe", testAuthor.getAuthor());
+        }
 
-    }
+        @Test
+        public void testBookTitleMethod() {
+            Book testTitle = new Book("Poe", "Paperback" , "Fiction");
+            assertEquals("The book is paperback", "Paperback", testTitle.getCoverType());
+            }
+        @Test
+        public void testWritingStyleMethod() {
+            Book testWriting = new Book("Harry Potter", "Thirty", "J K Rowling", "Hard Back", "Fiction");
+            assertEquals("The writing style is Fiction", "Fiction", testWriting.getWritingStyle());
+        }
+        @Test
+        public void testTitleMethod() {
+            Book testTitle = new Book("Harry Potter", "Thirty", "J K Rowling", "Hardback", "Fiction");
+            assertEquals("The book you are looking for is ", "Harry Potter", testTitle.getTitle());
+        }
+
+
 }
